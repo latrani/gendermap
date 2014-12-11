98d0b08dac56a7d7da743b03c786cdf4ff1b299d
Morgan Nichols <morgan@latrani.net>
2014-12-11 00:19:10 -0800

    Fix logic and convert to jQuery plugin

--- js/gendermap.js
+++ js/gendermap.js
@@ -1,42 +1,48 @@
-$(function(){
+(function ($) {
+  'use strict';
 
   var getDiamondCoords = function(event, $element){
     var height = $element.height();
     var offset = $element.offset();
-    var elementX = event.clientX - offset.left;
-    var elementY = event.clientY - offset.top;
+    var elementX = event.pageX - offset.left;
+    var elementY = event.pageY - offset.top;
     var radius = height/2;
     var diagonalF = elementX - elementY;
     var diagonalM = elementX + elementY;
     return {
-      "inside": (diagonalF > -radius) && (diagonalF < radius) &&
+      'inside': (diagonalF > -radius) && (diagonalF < radius) &&
                 (diagonalM > radius && diagonalM < (height + radius)),
-      "x": elementX,
-      "y": elementY,
-      "f": 1 - ((radius - diagonalF) / height),
-      "m": 1 - ((diagonalM - radius) / height)
+      'x': elementX,
+      'y': elementY,
+      'f': 1 - ((radius - diagonalF) / height),
+      'm': 1 - ((diagonalM - radius) / height)
     };
   };
 
-  $(".map-area").on("mousemove", function(event){
-    if (getDiamondCoords(event, $(this)).inside) {
-      $(this).css("cursor", "crosshair");
-    }
-    else {
-      $(this).css("cursor", "default");
-    }
-  });
+  $.fn.gendermap = function() {
+    var $mainEl = this;
+    var $maparea = $('<div class="gendermap-area"><div class="gendermap-marker">&times;</div></div>')
+      .on('mousemove', function(event){
+        if (getDiamondCoords(event, $(this)).inside) {
+          $(this).css('cursor', 'crosshair');
+        }
+        else {
+          $(this).css('cursor', 'default');
+        }
+      })
+      .on('click', function(event) {
+        var coords = getDiamondCoords(event, $(this));
+        if (coords.inside) {
+          $('.gendermap-marker').show().css({left: coords.x, top: coords.y});
+          $mainEl
+            .data('gendermap', {
+              m: coords.m,
+              f: coords.f
+            })
+            .trigger('mapClick');
+        }
+      });
 
-  $(".map-area").on("click", function(event) {
-    var coords = getDiamondCoords(event, $(this));
-    if (coords.inside) {
-      $(".user-value-male").text((Math.floor(coords.m * 21) * 5) + "%");
-      $(".user-value-female").text((Math.floor(coords.f * 21) * 5) + "%");
-      $("#gender-map-male").val(coords.m);
-      $("#gender-map-male-quantized").val(Math.ceil(coords.m * 5));
-      $("#gender-map-female").val(coords.f);
-      $("#gender-map-female-quantized").val(Math.ceil(coords.f * 5));
-      $(".marker").show().css({left: coords.x, top: coords.y});
-    }
-  });
-});
+    return $mainEl.addClass('gendermap-container').append($maparea);
+  };
+}(jQuery));
